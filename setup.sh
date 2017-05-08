61eadfd4c189cd39358f291e99515d011ba552c3
Alexsandr Kibal <ak@subgraph.com>
2017-05-08 12:00:42 +0000

    Install other packages

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt install -y git debhelper lintian diffoscope
 sudo apt install -y pbuilder debootstrap devscripts config-package-dev
 sudo apt install -y apt-cacher-ng cowbuilder git-buildpackage pristine-tar
 
