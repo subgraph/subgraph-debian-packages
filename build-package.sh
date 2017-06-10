1ee39f9bc64a7c9922154dccefbf043caf0513db
Alexsandr Kibal <ak@subgraph.com>
2017-06-10 18:02:42 +0000

    build one package by name

---
+++ build-package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+export ARCH=amd64
+export DIST=testing
+package="$1"
+gbp_args="buildpackage -us -uc"
+
+echo "building $package...";
+cd tmp/$package 
+git checkout debian;
+gbp $gbp_args;
+echo "done building package: $package";
