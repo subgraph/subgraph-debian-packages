7d70c85046249795cb4d5479c76e5ba776e1a1bf
Alexsandr Kibal <ak@subgraph.com>
2017-09-10 18:36:53 +0000

    explictly add basepath

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ sudo apt install -y pbuilder debootstrap devscripts config-package-dev
 sudo apt install -y apt-cacher-ng cowbuilder git-buildpackage pristine-tar
 
 DIST=stretch git-pbuilder create \
-        --mirror=http://127.0.0.1:3142/ftp.ca.debian.org/debian
+        --mirror=http://127.0.0.1:3142/ftp.ca.debian.org/debian \
+        --basepath /var/cache/pbuilder/base-stretch.cow
 sudo DIST=stretch /usr/sbin/cowbuilder --execute cow-setup.sh \
         --basepath /var/cache/pbuilder/base-stretch.cow \
         --save
